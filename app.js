485a97183448af73eae30fb5853dacc28170f208
Derrik Campbell <derrikcampbell@hotmail.com>
2019-09-23 18:49:53 -0400

    setting everything up

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-const http = require('http');
-
-const hostname = '127.0.0.1'; // request to got to that location
+const express = require('express');
+// const http = require('http');
+// const hostname = '127.0.0.1'; // request to got to that location
 const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\, we out here\n');   //this is the response
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.get('/contact', (req, res) => {
+    res.send('on the contact page');
+})
+
+app.get('/portfolio', (req, res) => {
+    res.send('on portfolio page');
+})
+
+app.listen(port, () => {
+    console.log(`Server runnin at ${port}`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`); //server running here
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\, we out here\n');   //this is the response
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`); //server running here
+// });
